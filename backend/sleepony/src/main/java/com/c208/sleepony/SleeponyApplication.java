a836c9ac4437eb91152e44b1d2e3343a3bbd0dc8
박성원 <mkos47635@naver.com>
2025-04-24 16:12:40 +0900

    :sparkles: Init: 백엔드 초기 설정

---
+++ backend/sleepony/src/main/java/com/c208/sleepony/SleeponyApplication.java
@@ -0,0 +1,13 @@
+package com.c208.sleepony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleeponyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleeponyApplication.class, args);
+	}
+
+}
---
+++ backend/sleepony/src/test/java/com/c208/sleepony/SleeponyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c208.sleepony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleeponyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
