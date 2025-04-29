247335a0e8a4e4e92ecf47c3cf0ce1ac0cf9deb6
박성원 <mkos47635@naver.com>
2025-04-29 10:47:09 +0900

    ✨ Feat: 스프링 시큐리티 설정 및 JWT 인증 필터 추가

--- backend/sleephony/src/main/java/com/c208/sleephony/global/config/SecurityConfig.java
+++ backend/sleephony/src/main/java/com/c208/sleephony/global/config/SecurityConfig.java
@@ -1,22 +1,37 @@
 package com.c208.sleephony.global.config;
 
+import com.c208.sleephony.global.security.JwtAuthenticationFilter;
+import com.c208.sleephony.global.utils.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // 모든 요청 허용
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/api/auth/login-google"
+                        ).permitAll()
+                        .anyRequest().authenticated()
                 )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
