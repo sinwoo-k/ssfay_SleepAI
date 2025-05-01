677a0a0b8f408f7484a88e295790eb7e4256ac11
hongseok <hghg990124@naver.com>
2025-05-02 00:33:14 +0900

    ✨_Feat:_wear os application 통신 테스트 중

--- frontend/Sleephony/sleephony/build.gradle.kts
+++ frontend/Sleephony/sleephony/build.gradle.kts
@@ -40,8 +40,6 @@ android {
 
 dependencies {
 
-
-
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
     // 앱 통싱
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
