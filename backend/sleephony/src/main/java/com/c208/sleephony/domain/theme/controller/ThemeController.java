6b3cc77b732ea392ea7d28574655b78f45f99eb8
박성원 <mkos47635@naver.com>
2025-05-02 13:57:33 +0900

    ✨ Feat: 테마 목록 조회

--- backend/sleephony/src/main/java/com/c208/sleephony/domain/theme/controller/ThemeController.java
+++ backend/sleephony/src/main/java/com/c208/sleephony/domain/theme/controller/ThemeController.java
@@ -1,4 +1,29 @@
 package com.c208.sleephony.domain.theme.controller;
 
+import com.c208.sleephony.domain.theme.entity.Theme;
+import com.c208.sleephony.domain.theme.service.ThemeService;
+import com.c208.sleephony.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/themes")
+@RequiredArgsConstructor
+@Tag(name = "Theme", description = "테마 관련 API")
 public class ThemeController {
+
+    private final ThemeService themeService;
+
+    @Operation(summary = "테마 목록 조회")
+    @GetMapping
+    public ApiResponse<List<Theme>> getAllThemes() {
+        List<Theme> themes = themeService.getAllThemes();
+        return ApiResponse.success(HttpStatus.OK, themes);
+    }
+
 }
--- backend/sleephony/src/main/java/com/c208/sleephony/domain/theme/service/ThemeService.java
+++ backend/sleephony/src/main/java/com/c208/sleephony/domain/theme/service/ThemeService.java
@@ -1,4 +1,20 @@
 package com.c208.sleephony.domain.theme.service;
 
+import com.c208.sleephony.domain.theme.entity.Theme;
+import com.c208.sleephony.domain.theme.repository.ThemeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ThemeService {
+
+    private final ThemeRepository themeRepository;
+
+    // 테마 전체 목록 조회
+    public List<Theme> getAllThemes() {
+        return themeRepository.findAll();
+    }
+
 }
