f59c1dfa9be0f2261e23fcf064a2398b90e4f0c4
hongs <hghg990124@naver.com>
2025-05-08 17:57:30 +0900

    ✨_Feat:_디테일 페이지 작성

--- frontend/Sleephony/app/src/main/java/com/example/sleephony/ui/screen/report/week/WeekREMSleepTime.kt
+++ frontend/Sleephony/app/src/main/java/com/example/sleephony/ui/screen/report/week/WeekREMSleepTime.kt
@@ -1,12 +1,17 @@
 package com.example.sleephony.ui.screen.report.week
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -21,11 +27,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.sleephony.R
 
 @Composable
 fun WeekREMSleepTime(
-    modifier: Modifier
+    modifier: Modifier,
+    navController: NavController
 ) {
     Column(
         modifier = modifier.fillMaxWidth(),
@@ -44,10 +52,30 @@ fun WeekREMSleepTime(
             .background(color = Color.Black.copy(alpha = .3f), shape = RoundedCornerShape(20.dp))
             .fillMaxWidth()
             .height(200.dp)
+            .clickable {
+                navController.navigate("detail/sleep_REM")
+            }
         ) {
             val days = remember { listOf("월", "화", "수", "목", "금", "토", "일") }
             val sleepHours = remember { listOf(1.6f, 1f, 1.3f, 1.4f, 1.3f, 1.8f, 1.2f) }
-            WeekChart(modifier = Modifier.fillMaxSize(), days = days, sleepHours = sleepHours)
+            Row(
+                modifier = modifier.fillMaxWidth()
+                    .padding(end = 15
+                        .dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.more_icon),
+                    contentDescription = "더보기 아이콘",
+                    modifier = modifier.size(30.dp),
+                )
+            }
+            WeekChart(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top=15.dp),
+                days = days,
+                sleepHours = sleepHours)
         }
     }
 }
