5b6536f83e6a66e8d617a1be6c58ca702cbda775
hongs <hghg990124@naver.com>
2025-05-08 17:57:30 +0900

    ✨_Feat:_디테일 페이지 작성

--- frontend/Sleephony/app/src/main/java/com/example/sleephony/ui/screen/report/week/WeekCalendar.kt
+++ frontend/Sleephony/app/src/main/java/com/example/sleephony/ui/screen/report/week/WeekCalendar.kt
@@ -1,11 +1,18 @@
 package com.example.sleephony.ui.screen.report.week
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
