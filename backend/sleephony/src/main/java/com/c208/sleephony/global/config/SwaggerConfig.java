fa4ee3c4938641163960f56644870c845da845aa
김도현 <kdhehsl@gmail.com>
2025-05-08 15:15:03 +0900

    ♻️ Refactor: Cors 부분 Swagger 추가 처리

--- backend/sleephony/src/main/java/com/c208/sleephony/global/config/SwaggerConfig.java
+++ backend/sleephony/src/main/java/com/c208/sleephony/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +22,7 @@ public class SwaggerConfig {
                         .version("1.0.0")
                         .description("API 명세서입니다."))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .addServersItem(new Server().url("/"))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
                                 new SecurityScheme()
