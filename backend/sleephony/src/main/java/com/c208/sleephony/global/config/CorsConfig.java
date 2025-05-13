2f2138c96ecbef77efb31e4d37809bc0d2d32828
박성원 <mkos47635@naver.com>
2025-05-13 12:02:03 +0900

    🚑️ HOTFIX: PATCH 추가

--- backend/sleephony/src/main/java/com/c208/sleephony/global/config/CorsConfig.java
+++ backend/sleephony/src/main/java/com/c208/sleephony/global/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("http://localhost:5173","https://k12c208.p.ssafy.io")); // 개발 시에는 * 사용 가능 (운영에서는 정확한 origin 지정)
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // Authorization 헤더 사용을 위함
 
