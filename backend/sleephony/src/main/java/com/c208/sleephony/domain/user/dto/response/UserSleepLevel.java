f7320ac62ad03d34e42b3a78f1db0fc8fcea2bf5
김도현 <kdhehsl@gmail.com>
2025-05-19 17:45:29 +0900

    ✨ Feat: 시연용 백엔드 API 제작

---
+++ backend/sleephony/src/main/java/com/c208/sleephony/domain/user/dto/response/UserSleepLevel.java
@@ -0,0 +1,12 @@
+package com.c208.sleephony.domain.user.dto.response;
+
+import com.c208.sleephony.domain.sleep.entity.SleepLevel;
+import com.c208.sleephony.domain.sleep.entity.SleepStage;
+
+import java.time.LocalDateTime;
+
+public record UserSleepLevel(LocalDateTime time, SleepStage level) {
+    public static UserSleepLevel fromEntity(SleepLevel e) {
+        return new UserSleepLevel(e.getMeasuredAt(), e.getLevel());
+    }
+}
