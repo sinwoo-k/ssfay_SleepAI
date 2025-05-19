cc0e1c3970081ef85e840df0d756699f420d9425
KimJiwon <rkl45687@gmail.com>
2025-05-19 15:00:17 +0900

    ✨Feat: 연동 워치 확인 & 가이드

--- frontend/Sleephony/app/src/main/java/com/example/sleephony/ui/screen/settings/SettingViewModel.kt
+++ frontend/Sleephony/app/src/main/java/com/example/sleephony/ui/screen/settings/SettingViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.sleephony.ui.screen.settings
 
+import android.content.Context
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,19 +8,23 @@ import com.example.sleephony.data.datasource.local.UserLocalDataSource
 import com.example.sleephony.data.model.user.UserProfileRequest
 import com.example.sleephony.data.model.user.UserProfileResult
 import com.example.sleephony.domain.repository.UserRepository
-import com.example.sleephony.navigation.ProfileStep
 import com.example.sleephony.utils.TokenProvider
+import com.google.android.gms.wearable.Node
+import com.google.android.gms.wearable.Wearable
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingViewModel @Inject constructor(
     private val userLocalDataSource: UserLocalDataSource,
     private val tokenProvider: TokenProvider,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    @ApplicationContext private val appContext: Context
 ) : ViewModel(){
 
     companion object {
@@ -37,8 +42,12 @@ class SettingViewModel @Inject constructor(
     private val _profileState = MutableStateFlow(DEFAULT_PROFILE)
     val profileState: StateFlow<UserProfileResult> = _profileState
 
+    private val _wearableNodes = MutableStateFlow<List<Node>>(emptyList())
+    val wearableNodes: StateFlow<List<Node>> = _wearableNodes
+
     init {
         _profileState.value = userLocalDataSource.getProfile()
+        fetchConnectedWearOsNodes()
     }
 
     fun logout () {
@@ -99,5 +108,22 @@ class SettingViewModel @Inject constructor(
             Log.d("DBG", "$response")
             logout()
         }
+    }
+
+    private fun fetchConnectedWearOsNodes() {
+        viewModelScope.launch {
+            try {
+                val nodeClient = Wearable.getNodeClient(appContext)
+                val nodes: List<Node> = nodeClient.connectedNodes.await()
+                _wearableNodes.value = nodes
+                Log.d("Wearable", "연결된 기기 수: ${nodes.size}")
+            } catch (e: Exception) {
+                Log.e("Wearable", "웨어러블 기기 확인 실패")
+            }
+        }
+    }
+
+    fun refreshConnectedWearOsNodes() {
+        fetchConnectedWearOsNodes()
     }
 }
